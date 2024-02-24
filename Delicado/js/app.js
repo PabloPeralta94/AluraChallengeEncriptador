caa1d9c0ec61103c22c5f15b84d5895b1f937cf3
unknown <pablo.peralta94@gmail.com>
2024-02-24 19:09:27 -0300

    arregla errores

--- Delicado/js/app.js
+++ Delicado/js/app.js
@@ -1,5 +1,6 @@
 const encryptText = document.querySelector(".user-text");
 const copyText = document.querySelector(".inputCopy");
+var regex = /^[a-z \n]+$/;
 
 const textFound = document.querySelector(".text-found");
 const notFound = document.querySelector(".not-found");
@@ -19,19 +20,25 @@ document.addEventListener("keyup", () => {
 });
 
 function encrypt() {
-  if (encryptText.value) {
-    message = encryptText.value
-      .replaceAll("e", "enter")
-      .replaceAll("i", "imes")
-      .replaceAll("a", "ai")
-      .replaceAll("u", "ufat")
-      .replaceAll("o", "ober");
+  if (!regex.test(encryptText.value)) {
+    return alert("No se permiten Mayusculas o caracteres especiales");
+  } 
 
-    copyText.value = message;
-    encryptText.value = "";
-    textFound.classList.remove("display-none");
-    notFound.classList.add("display-none");
-  } else {
+  else if(encryptText.value ){
+    message = encryptText.value
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("a", "ai")
+    .replaceAll("u", "ufat")
+    .replaceAll("o", "ober");
+
+  copyText.value = message;
+  encryptText.value = "";
+  textFound.classList.remove("display-none");
+  notFound.classList.add("display-none");
+
+  }
+  else {
     textFound.classList.add("display-none");
     notFound.classList.remove("display-none");
   }
